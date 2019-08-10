1db0d44110b23d4bebd18ca97827e72e8b02a777
huangelina5 <huangelina5@gmail.com>
2019-08-09 22:39:35 -0400

    lesson pushed

--- homework/8:4:19/August_4_2019_2.java
+++ homework/8:4:19/August_4_2019_2.java
@@ -1,33 +1,59 @@
 public class August_4_2019_2{
   public static void main(String[] args){
+    student s1 = New student();
+    s1.setName("john");
+    System.out.println(s1.getName())
 
   }
 }
 
 class student{
-  String name;
-  String address;
-  int[] numCourses = int [30];
-  int[] grades = int [30];
+  private String name;
+  private String address;
+  private int numCourses;
+  private String[] courses = String[30];
+  private int[] grades = int [30];
 
-  public student(String name, String address){
-    this.name = name;
-    this.address = address;
-    this.numCourses = numCourses;
-    this.grades = grades;
+  //public student(String name, String address){
+
+    public void setName(String name){
+      this.name = name;
+  }
+    public String getName(){
+      return this.name;
   }
 
-  public String getName(){
-    return this.name;
+  public void setAddress(String address){
+    this.address = address;
   }
 
   public String getAddress(){
     return this.address
   }
 
-  public void setAddress(String address){
-    this.address = address;
+  public void setNumcourses(int numCourses){
+    this numCourses = numCourses;
   }
+  public int getNumcourses(){
+    return this.numCourses;
+  }
+
+  public void setCourses(String[] courses){
+    this courses = courses;
+  }
+  public String[]  getCourses(){
+    return this.courses;
+  }
+  public void setGrade(int[] grades){
+    this grades = grades;
+  }
+  public int[]  getGrades(){
+    return this.grades;
+  }
+
+
+
+
 
   public String toString(){
     return "name(" + this.address + ")";
@@ -40,5 +66,14 @@ class student{
 
   public void printGrades(){
     System.out.println("name " + course1 + ":" + )
+  }
+
+  public double setAverageGrade(){
+
+  }
+
+
+  public double getAverageGrade(){
+
   }
 }
